ce041f82725478e25fe4fef7ead4e921cb8de568
Abhilaikh <id98621@gmail.com>
2021-10-05 15:47:15 +0530

    test_commit_1

---
+++ Practice1.py
@@ -0,0 +1,12 @@
+my_set = set()
+
+type(my_set)
+
+my_set.add(1)
+
+my_set.add(3)
+my_set.add(32)
+my_set.add(1)
+
+3>7
+
---
+++ readfilr2.py
@@ -0,0 +1,4 @@
+with open ('/Users/abhilaikh/Downloads/newfile.html',mode='r') as f:
+    print(f.read())
+    
+    print(f'This file was saved in path (abspath{f.name})')
